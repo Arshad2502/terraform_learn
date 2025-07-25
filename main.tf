80f531d15207dfd332dccd3b47cf64c79be62b02
Arshad2502 <arshad.jhs25@gmail.com>
2025-07-25 03:43:05 -0700

    terraform files

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "local" {}
+
+resource "local_file" "example_file1" {
+  content  = "This is the first file."
+  filename = "${path.module}/file1.txt"
+}
+
+resource "local_file" "example_file2" {
+  content  = "This is the second file."
+  filename = "${path.module}/file2.txt"
+}
+
+resource "null_resource" "create_dirs" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/dir1 ${path.module}/dir2"
+  }
+}
+
